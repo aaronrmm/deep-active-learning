1bb79826545ed96f184120c1e64dab5e62660753
Aaron <aaronrmm@gmail.com>
2019-09-07 09:04:17 -0400

    supports fastai type image setup

--- dataset.py
+++ dataset.py
@@ -1,8 +1,11 @@
 import numpy as np
 import torch
+import os
+from pathlib import Path
 from torchvision import datasets
 from torch.utils.data import Dataset
 from PIL import Image
+
 
 def get_dataset(name):
     if name == 'MNIST':
@@ -14,6 +17,7 @@ def get_dataset(name):
     elif name == 'CIFAR10':
         return get_CIFAR10()
 
+
 def get_MNIST():
     raw_tr = datasets.MNIST('./MNIST', train=True, download=True)
     raw_te = datasets.MNIST('./MNIST', train=False, download=True)
@@ -22,6 +26,7 @@ def get_MNIST():
     X_te = raw_te.test_data
     Y_te = raw_te.test_labels
     return X_tr, Y_tr, X_te, Y_te
+
 
 def get_FashionMNIST():
     raw_tr = datasets.FashionMNIST('./FashionMNIST', train=True, download=True)
@@ -32,6 +37,7 @@ def get_FashionMNIST():
     Y_te = raw_te.test_labels
     return X_tr, Y_tr, X_te, Y_te
 
+
 def get_SVHN():
     data_tr = datasets.SVHN('./SVHN', split='train', download=True)
     data_te = datasets.SVHN('./SVHN', split='test', download=True)
@@ -40,6 +46,7 @@ def get_SVHN():
     X_te = data_te.data
     Y_te = torch.from_numpy(data_te.labels)
     return X_tr, Y_tr, X_te, Y_te
+
 
 def get_CIFAR10():
     data_tr = datasets.CIFAR10('./CIFAR10', train=True, download=True)
@@ -50,6 +57,7 @@ def get_CIFAR10():
     Y_te = torch.from_numpy(np.array(data_te.test_labels))
     return X_tr, Y_tr, X_te, Y_te
 
+
 def get_handler(name):
     if name == 'MNIST':
         return DataHandler1
@@ -59,6 +67,7 @@ def get_handler(name):
         return DataHandler2
     elif name == 'CIFAR10':
         return DataHandler3
+
 
 class DataHandler1(Dataset):
     def __init__(self, X, Y, transform=None):
@@ -76,6 +85,7 @@ class DataHandler1(Dataset):
     def __len__(self):
         return len(self.X)
 
+
 class DataHandler2(Dataset):
     def __init__(self, X, Y, transform=None):
         self.X = X
@@ -92,6 +102,7 @@ class DataHandler2(Dataset):
     def __len__(self):
         return len(self.X)
 
+
 class DataHandler3(Dataset):
     def __init__(self, X, Y, transform=None):
         self.X = X
@@ -107,3 +118,24 @@ class DataHandler3(Dataset):
 
     def __len__(self):
         return len(self.X)
+
+
+class FastaiDataHandler(Dataset):
+    def __init__(self, train_dir, test_dir, transform=None):
+        assert os.path.isdir(train_dir), str("labeled_train_dir is not a dir")
+        assert os.path.isdir(test_dir), str("labeled_test_dir is not a dir")
+        self.transform = transform
+        self.X = []
+        self.Y = []
+        for class_dir in os.listdir(train_dir):
+            class_files = os.listdir(class_files)
+            self.X += [str(Path(train_dir, class_dir, img_file) for img_file in class_files)]
+            self.Y += [str(class_dir) for i in class_files]
+            assert (len(self.X) == len(self.Y))
+
+    def __getitem__(self, index):
+
+        x, y = Image(self.X[index]), Image(self.Y[index])
+        if self.transform is not None:
+            x = self.transform(x)
+        return x, y, index
--- run.py
+++ run.py
@@ -1,12 +1,13 @@
 import numpy as np
 from dataset import get_dataset, get_handler
 from model import get_net
+import config
 from torchvision import transforms
 import torch
 from query_strategies import RandomSampling, LeastConfidence, MarginSampling, EntropySampling, \
-                                LeastConfidenceDropout, MarginSamplingDropout, EntropySamplingDropout, \
-                                KMeansSampling, KCenterGreedy, BALDDropout, CoreSet, \
-                                AdversarialBIM, AdversarialDeepFool, ActiveLearningByLearning
+    LeastConfidenceDropout, MarginSamplingDropout, EntropySamplingDropout, \
+    KMeansSampling, KCenterGreedy, BALDDropout, CoreSet, \
+    AdversarialBIM, AdversarialDeepFool, ActiveLearningByLearning
 
 # parameters
 SEED = 1
@@ -15,43 +16,25 @@ NUM_INIT_LB = 10000
 NUM_QUERY = 1000
 NUM_ROUND = 10
 
-DATA_NAME = 'MNIST'
-# DATA_NAME = 'FashionMNIST'
-# DATA_NAME = 'SVHN'
-# DATA_NAME = 'CIFAR10'
+configuration = config.load_config()
+DATA_NAME = configuration.DATA_NAME
 
 args_pool = {'MNIST':
-                {'n_epoch': 10, 'transform': transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]),
-                 'loader_tr_args':{'batch_size': 64, 'num_workers': 1},
-                 'loader_te_args':{'batch_size': 1000, 'num_workers': 1},
-                 'optimizer_args':{'lr': 0.01, 'momentum': 0.5}},
-            'FashionMNIST':
-                {'n_epoch': 10, 'transform': transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]),
-                 'loader_tr_args':{'batch_size': 64, 'num_workers': 1},
-                 'loader_te_args':{'batch_size': 1000, 'num_workers': 1},
-                 'optimizer_args':{'lr': 0.01, 'momentum': 0.5}},
-            'SVHN':
-                {'n_epoch': 20, 'transform': transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.4377, 0.4438, 0.4728), (0.1980, 0.2010, 0.1970))]),
-                 'loader_tr_args':{'batch_size': 64, 'num_workers': 1},
-                 'loader_te_args':{'batch_size': 1000, 'num_workers': 1},
-                 'optimizer_args':{'lr': 0.01, 'momentum': 0.5}},
-            'CIFAR10':
-                {'n_epoch': 20, 'transform': transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616))]),
-                 'loader_tr_args':{'batch_size': 64, 'num_workers': 1},
-                 'loader_te_args':{'batch_size': 1000, 'num_workers': 1},
-                 'optimizer_args':{'lr': 0.05, 'momentum': 0.3}}
-            }
+                 {'n_epoch': 10,
+                  'transform': transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]),
+                  'loader_tr_args': {'batch_size': 64, 'num_workers': configuration.get_num_workers()},
+                  'loader_te_args': {'batch_size': 1000, 'num_workers': configuration.get_num_workers()},
+                  'optimizer_args': {'lr': 0.01, 'momentum': 0.5}}
+             }
 args = args_pool[DATA_NAME]
 
 # set seed
 np.random.seed(SEED)
 torch.manual_seed(SEED)
-torch.backends.cudnn.enabled = False
+#torch.backends.cudnn.enabled = False
 
 # load dataset
 X_tr, Y_tr, X_te, Y_te = get_dataset(DATA_NAME)
-X_tr = X_tr[:40000]
-Y_tr = Y_tr[:40000]
 
 # start experiment
 n_pool = len(Y_tr)
@@ -70,22 +53,15 @@ idxs_lb[idxs_tmp[:NUM_INIT_LB]] = True
 net = get_net(DATA_NAME)
 handler = get_handler(DATA_NAME)
 
-strategy = RandomSampling(X_tr, Y_tr, idxs_lb, net, handler, args)
+# strategy = RandomSampling(X_tr, Y_tr, idxs_lb, net, handler, args)
 # strategy = LeastConfidence(X_tr, Y_tr, idxs_lb, net, handler, args)
 # strategy = MarginSampling(X_tr, Y_tr, idxs_lb, net, handler, args)
 # strategy = EntropySampling(X_tr, Y_tr, idxs_lb, net, handler, args)
 # strategy = LeastConfidenceDropout(X_tr, Y_tr, idxs_lb, net, handler, args, n_drop=10)
 # strategy = MarginSamplingDropout(X_tr, Y_tr, idxs_lb, net, handler, args, n_drop=10)
-# strategy = EntropySamplingDropout(X_tr, Y_tr, idxs_lb, net, handler, args, n_drop=10)
-# strategy = KMeansSampling(X_tr, Y_tr, idxs_lb, net, handler, args)
-# strategy = KCenterGreedy(X_tr, Y_tr, idxs_lb, net, handler, args)
-# strategy = BALDDropout(X_tr, Y_tr, idxs_lb, net, handler, args, n_drop=10)
-# strategy = CoreSet(X_tr, Y_tr, idxs_lb, net, handler, args)
-# strategy = AdversarialBIM(X_tr, Y_tr, idxs_lb, net, handler, args, eps=0.05)
-# strategy = AdversarialDeepFool(X_tr, Y_tr, idxs_lb, net, handler, args, max_iter=50)
-# albl_list = [MarginSampling(X_tr, Y_tr, idxs_lb, net, handler, args),
-#              KMeansSampling(X_tr, Y_tr, idxs_lb, net, handler, args)]
-# strategy = ActiveLearningByLearning(X_tr, Y_tr, idxs_lb, net, handler, args, strategy_list=albl_list, delta=0.1)
+strategy = EntropySamplingDropout(X_tr, Y_tr, idxs_lb, net, handler, args, n_drop=10)
+
+
 
 # print info
 print(DATA_NAME)
@@ -95,11 +71,11 @@ print(type(strategy).__name__)
 # round 0 accuracy
 strategy.train()
 P = strategy.predict(X_te, Y_te)
-acc = np.zeros(NUM_ROUND+1)
-acc[0] = 1.0 * (Y_te==P).sum().item() / len(Y_te)
+acc = np.zeros(NUM_ROUND + 1)
+acc[0] = 1.0 * (Y_te == P).sum().item() / len(Y_te)
 print('Round 0\ntesting accuracy {}'.format(acc[0]))
 
-for rd in range(1, NUM_ROUND+1):
+for rd in range(1, NUM_ROUND + 1):
     print('Round {}'.format(rd))
 
     # query
@@ -112,7 +88,7 @@ for rd in range(1, NUM_ROUND+1):
 
     # round accuracy
     P = strategy.predict(X_te, Y_te)
-    acc[rd] = 1.0 * (Y_te==P).sum().item() / len(Y_te)
+    acc[rd] = 1.0 * (Y_te == P).sum().item() / len(Y_te)
     print('testing accuracy {}'.format(acc[rd]))
 
 # print results
